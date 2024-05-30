21d1b02a26e6a1c68e28b2d13e9fe3942c66f1b6
sarjithasarju <122516127+sarjithasarju@users.noreply.github.com>
2024-05-30 15:17:09 +0530

    Add files via upload

---
+++ sarjitha/30-05-2024/nd_array.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# Creating a 1-dimensional ndarray
+array_1d = np.array([1, 2, 3, 4, 5])
+print("1D array:")
+print(array_1d)
+
+# Creating a 2-dimensional ndarray
+array_2d = np.array([[1, 2, 3], [4, 5, 6]])
+print("\n2D array:")
+print(array_2d)
+
+# Creating a 3-dimensional ndarray
+array_3d = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+print("\n3D array:")
+print(array_3d)
