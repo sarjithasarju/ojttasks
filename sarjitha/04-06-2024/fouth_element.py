f0c93641c7192c539da0b879bc796efe7f80685a
sarjithasarju <122516127+sarjithasarju@users.noreply.github.com>
2024-06-04 16:04:43 +0530

    Add files via upload

---
+++ sarjitha/04-06-2024/fouth_element.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr=np.array([1,2,3,4,5,6,7,8])
+thirdElement=arr[2]
+fourthElement=arr[3]
+print("the third elemnt is ",thirdElement)
+print("the fourth element is ",fourthElement)
+addition=thirdElement+fourthElement
+print("sum of elements :",addition)
---
+++ sarjitha/04-06-2024/second_row.py
@@ -0,0 +1,6 @@
+import numpy as np
+arr=np.array([[1,2,3,4,5],
+              [6,7,8,9,10],
+              [11,12,13,14,14]])
+element=arr[1:2,4]
+print(element)
