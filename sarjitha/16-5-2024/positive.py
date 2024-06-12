55c4fd5c4841c65f1918f2f7a142523849951174
sarjithasarju <122516127+sarjithasarju@users.noreply.github.com>
2024-06-12 16:57:48 +0530

    Delete sarjitha/16-5-2024 directory

--- sarjitha/16-5-2024/positive.py
+++
@@ -1,7 +0,0 @@
-number=input("enter a number :")
-if number > 0:
-    print ("the number is positive")
-elif number < 0:
-    print ("the number is negative")
-else:
-    print ("the number is zero")
